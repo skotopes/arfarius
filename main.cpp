454ab416a9f2e26901e2859f4dfbb4d0cbdc25f1
Alexandr Kutuzov <alleteam@gmail.com>
2015-02-06 18:09:52 +0900

    load font on startup

--- main.cpp
+++ main.cpp
@@ -1,19 +1,16 @@
 #include "arfariusapplication.h"
 #include "mainwindow.h"
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
-#ifdef Q_OS_MACX
-    if ( QSysInfo::MacintoshVersion > QSysInfo::MV_10_8 ) {
-        QFont::insertSubstitution(".Lucida Grande UI", "Lucida Grande");
-    }
-#endif
-
     ArfariusApplication a(argc, argv);
     a.setOrganizationName("Plooks Ltd");
     a.setOrganizationDomain("plooks.com");
     a.setApplicationName("Arfarius");
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+    QFontDatabase::addApplicationFont("://fonts/FontAwesome.otf");
 
     MainWindow m(&a);
     m.show();
